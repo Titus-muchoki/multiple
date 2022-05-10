ef95d0c8c0f2aabd64daa5256c3862d76333e2f9
kajela <titoyut56@gmail.com>
2022-05-10 06:47:58 -0400

    Initial commit

---
+++ src/multiple.java
@@ -0,0 +1,18 @@
+public class multiple {
+    public void myCar() {
+        System.out.println("driving my x6m.competition");
+    }
+    public void mySpeed(int maxSpeed) {
+        System.out.println("the max speed is: "+maxSpeed);
+
+    }
+    public class three extends multiple{
+       class second{
+            //public static void main(String[] args) {
+               // multiple myObj = new multiple();
+                //myObj.myCar();
+                //myObj.mySpeed(400);
+            }
+        }
+
+}
---
+++ src/second.java
@@ -0,0 +1,8 @@
+public class second {
+    //class second{
+        public static void main(String[] args) {
+            multiple myObj = new multiple();
+            myObj.myCar();
+            myObj.mySpeed(400);
+        }
+    }
